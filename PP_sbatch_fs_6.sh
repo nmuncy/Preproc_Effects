673e627c5c50f33fdc949f9e407c75f256c3a469
nmuncy <pingamormn@gmail.com>
2017-08-24 14:28:07 -0600

    freesurfer

---
+++ PP_sbatch_fs_6.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# written by Nathan Muncy on 8/11/17
+
+#SBATCH --time=20:00:00   # walltime
+#SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --mem-per-cpu=16gb   # memory per CPU core
+#SBATCH -J "PPfs"   # job name
+
+# Compatibility variables for PBS. Delete if not needed.
+export PBS_NODEFILE=`/fslapps/fslutils/generate_pbs_nodefile`
+export PBS_JOBID=$SLURM_JOB_ID
+export PBS_O_WORKDIR="$SLURM_SUBMIT_DIR"
+export PBS_QUEUE=batch
+
+# Set the max number of threads to use for programs using OpenMP. Should be <= ppn. Does nothing if the program doesn't use OpenMP.
+export OMP_NUM_THREADS=$SLURM_CPUS_ON_NODE
+
+
+
+subjDir=$1
+subj=$2
+
+FS.Function () {
+
+    recon-all \
+    -subjid $1 \
+    -all \
+    -sd $2 \
+    -notal-check \
+    -cw256
+
+}
+
+
+FS.Function $subj $subjDir
